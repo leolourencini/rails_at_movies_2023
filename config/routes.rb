ce536711fcf5899b843bb40b82c2053b3cc9e125
Leonardo Lourencini <leolourencini@hotmail.com>
2023-09-19 14:17:01 -0500

    Added Styling with Bulma

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'production_companies/index'
-  get 'production_companies/show'
-  get 'movies/index'
-  get 'movies/show'
+  # get 'production_companies/index'
+  # get 'production_companies/show'
+  # get 'movies/index'
+  # get 'movies/show'
+  resources :movies, only: %i[index show]
+  resources :production_companies, only: %i[index show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
